b1cadab750774e4ce738dba81a580799a9498ee1
apadams <andpadam1@gmail.com>
2024-01-29 07:47:18 -0600

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+pub mod events;
+mod game;
+mod main_menu;
+mod systems;
+
+use game::GamePlugin;
+use main_menu::MainMenuPlugin;
+
+use systems::*;
+
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_state::<AppState>()
+        .add_plugin(MainMenuPlugin)
+        .add_plugin(GamePlugin)
+        .add_startup_system(spawn_camera)
+        .add_system(transition_to_game_state)
+        .add_system(transition_to_main_menu_state)
+        .add_system(exit_game)
+        .add_system(handle_game_over)
+        .run();
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum AppState {
+    #[default]
+    MainMenu,
+    Game,
+    GameOver,
+}
+
+
